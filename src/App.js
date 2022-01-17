912ec6afe3bc1b3561192f01432658c0e7f761de
janjicdj <janjicdj9@gmail.com>
2022-01-17 14:38:25 +0100

    Nova Beleska

--- src/App.js
+++ src/App.js
@@ -3,78 +3,94 @@ import NavBar from "./components/NavBar";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Pocetna from "./components/Pocetna";
 import NovaBeleska from "./components/NovaBeleska";
-import {useEffect, useState} from "react";
+import { useState} from "react";
 
 
 function App() {
-
-    const [beleske,setBeleske]=useState([
+    const [beleske, setBeleske] = useState([
         {
-            "id":1,
-            "naslov":"Naslov 1",
-            "tekst":"Tekst 1",
-            "status":"zavrsen"
-        },  {
-            "id":2,
-            "naslov":"Naslov 2",
-            "tekst":"Tekst 2",
-            "status":"cekanje"
-        },  {
-            "id":3,
-            "naslov":"Naslov 3",
-            "tekst":"Tekst 3",
-            "status":"cekanje"
+            "id": 1,
+            "naslov": "Naslov 1",
+            "tekst": "Tekst 1",
+            "status": "zavrsen"
+        }, {
+            "id": 2,
+            "naslov": "Naslov 2",
+            "tekst": "Tekst 2",
+            "status": "cekanje"
+        }, {
+            "id": 3,
+            "naslov": "Naslov 3",
+            "tekst": "Tekst 3",
+            "status": "cekanje"
         },
     ]);
+    const [zavrseni, setZavrseni] = useState(vratiZavrsene());
+    const [ukupno, setUkupno] = useState(beleske.length);
 
-    const[zavrseni,setZavrseni]=useState(vratiZavrsene());
-    const[ukupno,setUkupno]=useState(beleske.length);
 
-    function vratiZavrsene(){
-        let br =0;
-        beleske.forEach((b)=>{
-            if(b.status==="zavrsen")
+    function vratiZavrsene() {
+        let br = 0;
+        beleske.forEach((b) => {
+            if (b.status === "zavrsen")
                 br++;
         });
         return br;
     }
 
-function zavrsi(id){
-    beleske.forEach((b)=>{
-            if(b.id===id){
-                b.status="zavrsen";
+    function zavrsi(id) {
+        beleske.forEach((b) => {
+            if (b.id === id) {
+                b.status = "zavrsen";
             }
         });
-    osveziPocetnu()
+        osveziPocetnu()
     }
 
-    function osveziPocetnu(){
-        beleske.forEach((b)=>console.log(b))
+    function osveziPocetnu() {
+        beleske.forEach((b) => console.log(b))
         setBeleske(beleske);
         setZavrseni(vratiZavrsene())
         setUkupno(beleske.length)
+
     }
 
- function obrisi(id){
-    for(let i=0;i<beleske.length;i++){
-        if(beleske[i].id===id){
-            beleske.splice(i,1);
-            break;
+    function obrisi(id) {
+        for (let i = 0; i < beleske.length; i++) {
+            if (beleske[i].id === id) {
+                beleske.splice(i, 1);
+                break;
+            }
         }
+        osveziPocetnu();
     }
-    osveziPocetnu();
- }
 
-  return (
-      <BrowserRouter>
-        <NavBar zavrseni={zavrseni} ukupno={ukupno}/>
-          <Routes>
-            <Route path="/" element={<Pocetna beleske={beleske} zavrsi={zavrsi} obrisi={obrisi}/>}/>
-            <Route path="/novaB" element={<NovaBeleska/>}/>
-          </Routes>
-      </BrowserRouter>
+    function generisiId() {
+        return beleske.length==0?1:beleske[beleske.length - 1].id + 1
+    }
 
-  );
+    function dodajBelesku(e) {
+        e.preventDefault();
+        beleske.push({
+            'id': generisiId(),
+            'naslov': e.target[0].value,
+            'tekst': e.target[1].value,
+            'status': 'cekanje',
+        })
+        osveziPocetnu();
+        e.target.reset();
+    }
+
+    return (
+        <BrowserRouter>
+            <NavBar zavrseni={zavrseni} ukupno={ukupno}/>
+            <Routes>
+                <Route path="/" element={<Pocetna beleske={beleske} zavrsi={zavrsi} obrisi={obrisi}/>}/>
+                <Route path="/novaB" element={<NovaBeleska dodajBelesku={dodajBelesku}/>}/>
+            </Routes>
+        </BrowserRouter>
+
+    );
 }
 
 export default App;
--- src/components/NovaBeleska.jsx
+++ src/components/NovaBeleska.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
-function NovaBeleska(props) {
+function NovaBeleska({dodajBelesku}) {
+    const stil={width:50+'vw',marginLeft:'auto',marginRight:'auto'}
     return (
         <div>
-            <h1>Nova beleska</h1>
+            <form style={stil} onSubmit={dodajBelesku}>
+                <h2> Dodaj novu belešku</h2>
+                <div className="mb-3">
+                    <label htmlFor="naslovB" className="form-label">Naslov</label>
+                    <input type="text" name="naslov" className="form-control" id="naslovB" />
+
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="sadrzajB" className="form-label">Sadržaj</label>
+                    <textarea id="sadrzajB" name="sadrzaj" className="form-control"></textarea>
+                </div>
+                <button type="submit" className="btn btn-primary">Sačuvaj</button>
+                <button type="reset" className="btn btn-secondary">Reset</button>
+            </form>
         </div>
     );
 }
